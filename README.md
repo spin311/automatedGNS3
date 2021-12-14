0c6bac10834994a239efbf3b2e51f24d0423a045
spin311 <svit.spindler@gmail.com>
2021-12-14 22:35:42 +0100

    Added firefox + vncserver install

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ Deployment and usage of GNS3 using a Virtual Machine.
 Instructions:
 1. Download Vagrant (Version >= 2.0.0)
 2. Open folder in terminal
-3. run: vagrant up
+3. type in terminal: vagrant up
+4. wait for everything to finish downloading
+5. log in:
+username: vagrant
+password: vagrant
+6. type in termninal (the one from VirtualMachine): reboot
--- Vagrant/skripta.sh
+++ Vagrant/skripta.sh
@@ -13,7 +13,12 @@ echo "installing gns3"
 add-apt-repository ppa:gns3/ppa -y
 apt-get install gns3-gui gns3-server  -y
 
-echo "finished downloading"
+echo "installing firefox"
+apt-get install firefox -y
+
+echo "installing vncserver"
+apt-get install tightvncserver -y
+
+echo "finished downloading, log in and then reboot"
 echo"username: vagrant"
 echo"password: vagrant"
-echo "REBOOTING"
