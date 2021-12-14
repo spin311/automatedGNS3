cd3eb62fee4704576a6db21f7795857a34f88319
spin311 <svit.spindler@gmail.com>
2021-12-14 17:36:26 +0100

    Added echo comments

--- Vagrant/skripta.sh
+++ Vagrant/skripta.sh
@@ -1,6 +1,11 @@
 export DEBIAN_FRONTEND=noninteractive
 
+echo "updating"
 apt-get update -y
+
+echo "upgrading"
 apt-get upgrade -y
+
+echo "installing gns3"
 add-apt-repository ppa:gns3/ppa -y
 apt-get install gns3-gui gns3-server  -y
