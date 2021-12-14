2b2d66395350f4e748572c019dcf2305b077af97
spin311 <svit.spindler@gmail.com>
2021-12-14 17:50:12 +0100

    Added gnome desktop

--- Vagrant/skripta.sh
+++ Vagrant/skripta.sh
@@ -9,3 +9,9 @@ apt-get upgrade -y
 echo "installing gns3"
 add-apt-repository ppa:gns3/ppa -y
 apt-get install gns3-gui gns3-server  -y
+
+echo "installing gnome"
+apt-get install gnome -y
+echo"finished downloading"
+echo"REBOOTING"
+reboot
