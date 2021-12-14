0681cb8f5b25f82325347e950fd3b1b6168b74cd
spin311 <svit.spindler@gmail.com>
2021-12-14 18:15:58 +0100

    replaced gnome with xfce4

--- Vagrant/skripta.sh
+++ Vagrant/skripta.sh
@@ -6,12 +6,15 @@ apt-get update -y
 echo "upgrading"
 apt-get upgrade -y
 
+echo "installing  xfce GUI"
+apt-get install -y xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+
 echo "installing gns3"
 add-apt-repository ppa:gns3/ppa -y
 apt-get install gns3-gui gns3-server  -y
 
-echo "installing gnome"
-apt-get install gnome -y
 echo "finished downloading"
+echo"username: vagrant"
+echo"password: vagrant"
 echo "REBOOTING"
 reboot
