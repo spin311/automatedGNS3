de321d5c8e04b77f14582f1b1fb201bb14c513a3
spin311 <svit.spindler@gmail.com>
2021-12-19 21:10:01 +0100

    Read me +bugfixes

--- Vagrant/skripta.sh
+++ Vagrant/skripta.sh
@@ -7,18 +7,20 @@ echo "upgrading"
 apt-get upgrade -y
 
 echo "installing  xfce GUI"
-apt-get install -y xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+apt-get install -y xfce4
 
 echo "installing gns3"
 add-apt-repository ppa:gns3/ppa -y
-apt-get install gns3-gui=2.2.27 gns3-server=2.2.27  -y
+apt-get install gns3-gui gns3-server  -y
 
 echo "installing firefox"
-apt-get install firefox -y
+apt-get install firefox -y 
 
 echo "installing vncserver"
 apt-get install tightvncserver -y
 
+usermod -aG ubridge vagrant
+ufw allow 5900
 echo "finished downloading, login and then reboot"
 echo "username: vagrant"
 echo "password: vagrant"
